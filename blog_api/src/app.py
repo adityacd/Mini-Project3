3d1e44396fb60b60b2a814ce5f48dd0478223fff
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2019-12-18 14:41:10 -0500

    Feature: update app.py

--- blog_api/src/app.py
+++ blog_api/src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 
 from .config import app_config
+from .models import db, bcrypt
 
 
 def create_app(env_name):
@@ -13,11 +14,14 @@ def create_app(env_name):
 
     app.config.from_object(app_config[env_name])
 
+    bcrypt.init_app(app)  # add this line
+    db.init_app(app)  # add this line
+
     @app.route('/', methods=['GET'])
     def index():
         """
         example endpoint
         """
-        return 'Congratulations! Your first endpoint is workin'
+        return 'Congratulations! Your first endpoint is working'
 
     return app
