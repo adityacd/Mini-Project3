ef597b55e1cff5068bd78c0d04d3c1c88565680b
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2019-12-20 17:10:42 -0500

    Fix: Syntax fix

--- blog_api/run.py
+++ blog_api/run.py
@@ -1,10 +1,10 @@
 import os
 
-from src.app import create_app
+from .src.app import create_app
 
-if __name__ == '__main__':
+if __name__ == '__main':
     env_name = os.getenv('FLASK_ENV')
     app = create_app(env_name)
-    # run app
-    app.run()
-
+    if __name__ == '__main__':
+        port = os.getenv('PORT')
+        app.run(host='0.0.0.0', port=port)
