2c43e887cbff9236647f5b694a0f3e229cbc36d5
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2019-12-20 16:04:08 -0500

    Task: Updated run.py

--- blog_api/run.py
+++ blog_api/run.py
@@ -1,10 +1,10 @@
 import os
-from .src.app import create_app
 
-env_name = os.getenv('FLASK_ENV')
-app = create_app(env_name)
+from src.app import create_app
 
-if __name__ == '__main__':
-    port = os.getenv('PORT')
-    # run app
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main':
+    env_name = os.getenv('FLASK_ENV')
+    app = create_app(env_name)
+    if __name__ == '__main__':
+        port = os.getenv('PORT')
+        app.run(host='0.0.0.0', port=port)
