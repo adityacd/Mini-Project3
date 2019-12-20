b0ea5d25af35f9c863d22e9df1d99b99c533b2a2
Aditya Deshmukh <37020817+adityacd@users.noreply.github.com>
2019-12-20 17:12:14 -0500

    Task: Updated app.py

--- blog_api/src/app.py
+++ blog_api/src/app.py
@@ -1,9 +1,11 @@
-# src/app.py
-
 from flask import Flask
 
 from .config import app_config
-from .models import db, bcrypt  # add this new line
+from .models import db, bcrypt
+from flask_sqlalchemy import SQLAlchemy
+
+from .views.UserView import user_api as user_blueprint
+from .views.BlogpostView import blogpost_api as blogpost_blueprint
 
 
 def create_app(env_name):
@@ -16,16 +18,17 @@ def create_app(env_name):
 
     app.config.from_object(app_config[env_name])
 
-    # initializing bcrypt
     bcrypt.init_app(app)  # add this line
-
     db.init_app(app)  # add this line
+
+    app.register_blueprint(user_blueprint, url_prefix='/api/v1/users')
+    app.register_blueprint(blogpost_blueprint, url_prefix='/api/v1/blogpost')
 
     @app.route('/', methods=['GET'])
     def index():
         """
         example endpoint
         """
-        return 'Congratulations! Your first endpoint is working'
+        return 'Congratulations! Your part 2 endpoint is working'
 
     return app
